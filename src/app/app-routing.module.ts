3c51f124e319069ddb7618643f0cdb1e89b96685
Chad Racelis <chadracelis@yahoo.com>
2019-12-08 17:57:47 -0500

    Added side-menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { HomePageModule } from './pages/home/home.module';
-import { StatsPageModule } from './pages/stats/stats.module';
-import { SearchPageModule } from './pages/search/search.module';
-import { VotePageModule } from './pages/vote/vote.module';
-
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => HomePageModule },
-  { path: 'stats', loadChildren: () => StatsPageModule },
-  { path: 'search', loadChildren: () => SearchPageModule },
-  { path: 'vote', children: [
-      { path: '', loadChildren: () =>  VotePageModule },
-  ]},
+  { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule' },
 ];
 
 @NgModule({
-  imports: [
-    HomePageModule,
-    StatsPageModule,
-    SearchPageModule,
-    VotePageModule,
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
