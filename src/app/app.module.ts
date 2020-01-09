d399800aa6cad1aa7453810930adb579bdc1f99f
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-01-09 13:58:43 +0800

    Added sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { RouteReuseStrategy } from '@angular/router';
@@ -14,6 +14,27 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://acb9a7ff020f4a4aacdb8a5e9e8214e4@sentry.io/1875730"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+
+  handleError(error) {
+    console.error("Globally catched exception:", error);
+
+    console.log(document.URL);
+    // Only send reports to sentry if we are not debugging.
+    if (document.URL.includes('localhost')) { // Prod builds or --nodebug CLI builds use "http://localhost"
+      Sentry.captureException(error.originalError || error);
+    }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -38,7 +59,7 @@ import { AppRoutingModule } from './app-routing.module';
     Platform,
     WebView,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {provide: ErrorHandler, useClass: ErrorHandler}
+    {provide: ErrorHandler, useClass: SentryErrorHandler}
   ]
 })
 export class AppModule {}
