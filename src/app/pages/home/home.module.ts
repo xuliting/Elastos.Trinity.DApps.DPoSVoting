bee40900b4f66e5053228e0f4f095abdf07878f4
Chad Racelis <chadracelis@yahoo.com>
2020-02-18 21:14:26 -0500

    Fix image delay in onboard pg

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    IonicImageLoader,
     RouterModule.forChild(routes)
   ],
   declarations: [HomePage]
